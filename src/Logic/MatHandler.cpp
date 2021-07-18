66e59949e6629116972298f6b2fcb3c24151e364
denis <orto.fosfat@yandex.ru>
2021-07-18 12:17:27 +0300

    update Mat Handler class with debug cout

--- src/Logic/MatHandler.cpp
+++ src/Logic/MatHandler.cpp
@@ -7,7 +7,7 @@
 #include <utility>
 #include "Logic/DotChecker.h"
 #include "Logic/Exception.h"
-#define SEARCH_RAD 7
+#define SEARCH_RAD 15
 
 MatHandler::MatHandler(std::shared_ptr<IOpenCVWrapper> wrapper, std::shared_ptr<IPathSearcher> pathSearcher):
 m_pWrapper(std::move(wrapper)),
@@ -21,6 +21,7 @@ void MatHandler::SetStartDot(QPoint dot) {
     }
     m_isStartSet = true;
     m_startDot = dot;
+    std::cout << "Start dot setted!!!   " << m_startDot.x() << ' ' << m_startDot.y() << std::endl;
 }
 
 void MatHandler::SetEndDot(QPoint dot) {
@@ -30,6 +31,7 @@ void MatHandler::SetEndDot(QPoint dot) {
     }
     m_isEndSet = true;
     m_endDot = dot;
+    std::cout << "End dot setted!!!   " << m_endDot.x() << ' ' << m_endDot.y() << std::endl;
 }
 
 int MatHandler::GetResultPix() {
@@ -37,7 +39,13 @@ int MatHandler::GetResultPix() {
         throw GetResultWithoutInitDotsException("start point not set");
     if (!m_isEndSet)
         throw GetResultWithoutInitDotsException("end point not set");
-    return m_pPathSearcher->GetNearestPath(m_startDot, m_endDot);
+    auto start = DotChecker::GetStartRoadDot(m_pWrapper, m_startDot, SEARCH_RAD);
+    auto end = DotChecker::GetStartRoadDot(m_pWrapper, m_endDot, SEARCH_RAD);
+    std::cout << "END: (" << end.x() << ", " << end.y() << ")\n";
+    auto res = m_pPathSearcher->GetNearestPath(start, end);
+    m_isStartSet = false;
+    m_isEndSet = false;
+    return res;
 }
 
 int MatHandler::GetResultMM() {
